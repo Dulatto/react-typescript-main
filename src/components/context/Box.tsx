01d573c3addd468061756749e2cee0de0e8b5371
Dulatto <dulat.omarov@gmail.com>
2022-02-22 22:39:10 -0500

    useContext

---
+++ src/components/context/Box.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Box = () => {
+    return (
+        <div>
+            Theme context
+        </div>
+    );
+};
+
---
+++ src/components/context/theme.ts
@@ -0,0 +1,10 @@
+export const theme ={
+    primary:{
+        main: '#ef51b5',
+        text: '#fff'
+    },
+    secondary:{
+        main: '#f50057',
+        text: '#fff'
+    },
+}
