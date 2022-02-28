25db05e4a473a59e77032525a7b9a9ab1b750f28
Dulatto <dulat.omarov@gmail.com>
2022-02-27 23:25:56 -0500

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -2,44 +2,44 @@ import React from 'react';
 import './App.css';
 import { Button } from './components/Button';
 import { Container } from './components/Container';
-import {Greet} from './components/Greet';
+import { Greet } from './components/Greet';
 import { Heading } from './components/Heading';
 import { Input } from './components/Input';
 import { Oscar } from './components/Oscar';
-import {Person} from './components/Person';
+import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
 import { Status } from './components/Status';
-import {ThemeContextProvider} from './components/context/ThemeContext'
-import {Box} from './components/context/Box'
-import {UserContextProvider} from './components/context/UseContext'
-import {User} from './components/context/User'
+import { ThemeContextProvider } from './components/context/ThemeContext'
+import { Box } from './components/context/Box'
+import { UserContextProvider } from './components/context/UseContext'
+import { User } from './components/context/User'
 import { MutableRef } from './components/ref/MutableRef';
 
 function App() {
 
-  const personName ={
-    first:'Bruce',
-    last: 'Wayne',
-  }
+  // const personName ={
+  //   first:'Bruce',
+  //   last: 'Wayne',
+  // }
 
-  const nameList =[
-    {
-      first: 'Bruce',
-      last: 'Wayne'
-    },
-    {
-      first: 'Clark',
-      last: 'Kent'
-    },
-    {
-      first: 'Princess',
-      last: 'Diana'
-    }
-  ]
-   
+  // const nameList =[
+  //   {
+  //     first: 'Bruce',
+  //     last: 'Wayne'
+  //   },
+  //   {
+  //     first: 'Clark',
+  //     last: 'Kent'
+  //   },
+  //   {
+  //     first: 'Princess',
+  //     last: 'Diana'
+  //   }
+  // ]
+
   return (
     <div className="App">
-       {/*<Container styles={{border: '1px solid black', padding: '1rem'}} />
+      {/*<Container styles={{border: '1px solid black', padding: '1rem'}} />
       <Button 
          handleClick={(event, id)=>{
         console.log('Button clicked', event, id)
